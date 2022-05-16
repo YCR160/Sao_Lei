9523af46f1e28391116e0295202daa4332a87e43
Ye CR <3342711246@qq.com>
2022-05-17 07:58:05 +0800

    增加了对雷区>10x10的情况的适配 优化了代码风格

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,30 +7,28 @@ namespace test
         const int field = 10;
         static int[,] lei = new int[field, field];
         static int num;
+
         /// <summary>
-        /// 数数的
+        /// 附近有多少个雷
         /// </summary>
         /// <param name="row">行</param>
         /// <param name="col">列</param>
         /// <returns></returns>
-        static int Count(int row, int col)
+        static void Count(int row, int col)
         {
-            if (lei[row, col] != 114) return -1;
-            int ans = 0;
+            if (lei[row, col] != 114) return;//已经数过或者是雷
+            lei[row, col] = 0;
             for (int i = row - 1; i < row + 2; i++)
             {
                 for (int j = col - 1; j < col + 2; j++)
                 {
                     if (i == row && j == col) continue;
-                    if (i > 9 || j > 9) continue;
-                    if (i < 0 || j < 0) continue;
-                    if (lei[i, j] == -1) ans++;
+                    if ((i > field - 1 || j > field - 1) || (i < 0 || j < 0)) continue;
+                    if (lei[i, j] == -1) lei[row, col]++;
                 }
             }
-            lei[row, col] = ans;
             num--;
-            if (ans == 0) Find(row, col);
-            return ans;
+            if (lei[row, col] == 0) Find(row, col);
         }
         static void Find(int row, int col)
         {
@@ -39,12 +37,8 @@ namespace test
                 for (int j = col - 1; j < col + 2; j++)
                 {
                     if (i == row && j == col) continue;
-                    if (i > 9 || j > 9) continue;
-                    if (i < 0 || j < 0) continue;
-                    if (lei[i, j] == 114)
-                    {
-                        Count(i, j);
-                    }
+                    if ((i > field - 1 || j > field - 1) || (i < 0 || j < 0)) continue;
+                    if (lei[i, j] == 114) Count(i, j);
                 }
             }
         }
@@ -60,50 +54,46 @@ namespace test
                         lei[i, j] = 114;//未知
                     }
                 }
-                Console.WriteLine("hai hai");
-                Console.WriteLine("输入一个坐标开始，坐标格式“a,b”");
+                Console.WriteLine(" hai hai \n 输入一个坐标开始，坐标格式“a,b” \n");
                 var point = new Point(Console.ReadLine());
                 num = 0;
                 while (num < field)
                 {
                     Random rnd = new Random();
-                    int row = rnd.Next(0, 10);
-                    int col = rnd.Next(0, 10);
+                    int row = rnd.Next(0, field);
+                    int col = rnd.Next(0, field);
                     if (lei[row, col] == 114 && !(row == point.X && col == point.Y))
                     {
                         lei[row, col] = -1;//埋雷
                         num++;
                     }
                 }
-                num = 90;
-                lei[point.X, point.Y] = Count(point.X, point.Y);
+                num = field * field - 10;
+                Count(point.X, point.Y);
                 while (true)
                 {
-                    if (num == 0) Console.WriteLine(" 你好扫啊 ");
-                    Console.WriteLine("  0 1 2 3 4 5 6 7 8 9");
-                    for (int i = 0; i < 10; i++)
+                    Console.Write("    ");
+                    for (int i = 0; i < field; i++) Console.Write(i < 10 ? "  " + i : " " + i);
+                    Console.Write("\n    ");
+                    for (int i = 0; i < field; i++) Console.Write("---");
+                    for (int i = 0; i < field; i++)
                     {
-                        Console.Write(i + " ");
-                        for (int j = 0; j < 10; j++)
+                        Console.Write("\n" + i + (i < 10 ? "  | " : " | "));
+                        for (int j = 0; j < field; j++)
                         {
-                            if (lei[i, j] < 0 || lei[i, j] == 114) Console.Write("▅");
-                            else Console.Write(lei[i, j]);
-                            Console.Write(" ");
+                            if (lei[i, j] < 0 || lei[i, j] == 114) Console.Write("▅  ");
+                            else Console.Write(lei[i, j] + "  ");
                         }
-                        Console.Write("\n");
                     }
                     point = new Point(Console.ReadLine());
-                    if (point.X < 0 || point.Y < 0) continue;
-                    if (point.X > 9 && point.Y > 9) continue;
+                    if ((point.X > field - 1 || point.Y > field - 1) || (point.X < 0 || point.Y < 0)) continue;
                     if (lei[point.X, point.Y] == -1)
                     {
-                        Console.WriteLine("ji");
+                        Console.WriteLine("寄");
                         break;
                     }
-                    else
-                    {
-                        Count(point.X, point.Y);
-                    }
+                    else Count(point.X, point.Y);
+                    if (num == 0) Console.WriteLine(" 你好扫啊 ");
                 }
             }
             Console.ReadKey();
